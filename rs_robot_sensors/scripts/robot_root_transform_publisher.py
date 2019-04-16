f68b46c862971f1b2f5e268b9a12fadae0aafb36
Yusuke Furuta <tarukosu0@gmail.com>
2019-04-16 11:17:11 +0900

    add t265 ground offset frame

--- rs_robot_sensors/scripts/robot_root_transform_publisher.py
+++ rs_robot_sensors/scripts/robot_root_transform_publisher.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python  
+#!/usr/bin/env python
 import rospy
 
 # Because of transformations
@@ -6,39 +6,13 @@ import tf_conversions
 
 import tf2_ros
 import geometry_msgs.msg
-import turtlesim.msg
-
-
-def handle_turtle_pose(msg, turtlename):
-    br = tf2_ros.TransformBroadcaster()
-    broadcaster = tf2_ros.StaticTransformBroadcaster()
-
-    t = geometry_msgs.msg.TransformStamped()
-
-    t.header.stamp = rospy.Time.now()
-    t.header.frame_id = "world"
-    t.child_frame_id = turtlename
-    t.transform.translation.x = msg.x
-    t.transform.translation.y = msg.y
-    t.transform.translation.z = 0.0
-    q = tf_conversions.transformations.quaternion_from_euler(0, 0, msg.theta)
-    t.transform.rotation.x = q[0]
-    t.transform.rotation.y = q[1]
-    t.transform.rotation.z = q[2]
-    t.transform.rotation.w = q[3]
-
-    br.sendTransform(t)
 
 def publish_transform(transform, base_frame, parent_frame, child_frame):
     broadcaster = tf2_ros.StaticTransformBroadcaster()
-
-    # t = geometry_msgs.msg.TransformStamped()
-
     transform.header.stamp = rospy.Time.now()
     transform.header.frame_id = parent_frame
     transform.child_frame_id = child_frame
 
-    print(transform)
     broadcaster.sendTransform(transform)
 
 if __name__ == '__main__':
@@ -47,12 +21,6 @@ if __name__ == '__main__':
     tfBuffer = tf2_ros.Buffer()
     listener = tf2_ros.TransformListener(tfBuffer)
 
-    # rospy.wait_for_service('spawn')
-    # spawner = rospy.ServiceProxy('spawn', turtlesim.srv.Spawn)
-    # turtle_name = rospy.get_param('turtle', 'turtle2')
-    # spawner(4, 2, 0, turtle_name)
-
-    # turtle_vel = rospy.Publisher('%s/cmd_vel' % turtle_name, geometry_msgs.msg.Twist, queue_size=1)
     base_frame = rospy.get_param('~base_frame', 'odom')
     parent_frame = rospy.get_param('~parent_frame', 'parent_frame')
     child_frame = rospy.get_param('~child_frame', 'child_frame')
@@ -76,15 +44,3 @@ if __name__ == '__main__':
 
     while not rospy.is_shutdown():
         rate.sleep()
-
-    """
-        msg = geometry_msgs.msg.Twist()
-
-        msg.angular.z = 4 * math.atan2(trans.transform.translation.y, trans.transform.translation.x)
-        msg.linear.x = 0.5 * math.sqrt(trans.transform.translation.x ** 2 + trans.transform.translation.y ** 2)
-
-        turtle_vel.publish(msg)
-
-        rate.sleep()
-    """
-
---
+++ rs_robot_sensors/scripts/t265_ground_offset_publisher.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python  
+import rospy
+
+import tf2_ros
+import geometry_msgs.msg
+
+def publish_transform(transform, base_frame, parent_frame, child_frame):
+    broadcaster = tf2_ros.StaticTransformBroadcaster()
+
+    transform.header.stamp = rospy.Time.now()
+    transform.header.frame_id = parent_frame
+    transform.child_frame_id = child_frame
+
+    print(transform)
+    broadcaster.sendTransform(transform)
+
+
+def publish_transform(parent_frame, child_frame, offset_z):
+    broadcaster = tf2_ros.StaticTransformBroadcaster()
+
+    t = geometry_msgs.msg.TransformStamped()
+
+    t.header.stamp = rospy.Time.now()
+    t.header.frame_id = parent_frame
+    t.child_frame_id = child_frame
+    t.transform.translation.x = 0.0
+    t.transform.translation.y = 0.0
+    t.transform.translation.z = offset_z
+    t.transform.rotation.x = 0.0
+    t.transform.rotation.y = 0.0
+    t.transform.rotation.z = 0.0
+    t.transform.rotation.w = 1.0
+
+    broadcaster.sendTransform(t)
+
+if __name__ == '__main__':
+    rospy.init_node('t265_ground_offset_publisher')
+
+    tfBuffer = tf2_ros.Buffer()
+    listener = tf2_ros.TransformListener(tfBuffer)
+
+    default_offset = rospy.get_param('~default_offset', 0.0)
+    base_frame = rospy.get_param('~base_frame', 'odom')
+    footprint_frame = rospy.get_param('~footprint_frame', 'base_footprint')
+    child_frame = rospy.get_param('~child_frame', 'child_frame')
+    update_rate = rospy.get_param('~update_rate', 0.1) # Hz
+
+    rospy.loginfo("default_offset: " + str(base_frame))
+    
+    rospy.loginfo("base_frame: " + base_frame)
+    rospy.loginfo("footprint_frame: " + footprint_frame)
+    rospy.loginfo("child_frame: " + child_frame)
+
+    publish_transform(base_frame, child_frame, default_offset)
+    
+    rospy.sleep(10.0)
+
+    rate = rospy.Rate(update_rate)
+    while not rospy.is_shutdown():
+        try:
+            transform = tfBuffer.lookup_transform(base_frame, footprint_frame, rospy.Time())
+            print(transform)
+            offset_z = -transform.transform.translation.z
+            publish_transform(base_frame, child_frame, offset_z)
+            rate.sleep()
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+            rate.sleep()
+            continue
+
